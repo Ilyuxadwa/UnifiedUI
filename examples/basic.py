1a9c8d27ed26e867462db2634ad891f7db03654b
Ilyuxadwa <ilyuxadwa.work@gmail.com>
2026-06-03 23:15:53 +0300

    Update example

--- examples/basic.py
+++ examples/basic.py
@@ -17,9 +17,15 @@ example_settings_category.add_slider("slider", "Example Slider", 0.5, min_value=
 example_settings_category.add_directory("directory", "Example Directory", on_change=lambda value: print(f"Directory changed to: {value}"))
 
 def ui(app):
+
+    table = tools.Table(app, 12).cols("Column 1", "Column 2", "Column 3").add_row("Some 1", "Information 1", "Here 1").add_row("Some 2", "Information 2", "Here 2")
+    table.size(w=80, h=16)
+    
     app.add_element(app.alignment("rcenter", 
                         ft.Button("Main Button", width = tools.adapt_dimensions(app, "w", 16), height = tools.adapt_dimensions(app, "h", 8), style=tools.main_button_style(app, 12, padding = 5), icon=ft.Icons.SETTINGS_ROUNDED, on_click=lambda _: print("Button clicked!")),
                         ft.Button("Additional Button", width = tools.adapt_dimensions(app, "w", 14), height = tools.adapt_dimensions(app, "h", 10), style=tools.additional_button_style(app, 12, padding = 5), icon=ft.Icons.SETTINGS_ROUNDED, on_click=lambda _: print("Button clicked!")),
+                        ft.Button("Okay", width = tools.adapt_dimensions(app, "w", 12), height = tools.adapt_dimensions(app, "h", 6), style=tools.ok_button_style(app, 12, padding = 5)),
+                        ft.Button("Cancel", width = tools.adapt_dimensions(app, "w", 12), height = tools.adapt_dimensions(app, "h", 6), style=tools.cancel_button_style(app, 12, padding = 5)),
                     expand=True))
     app.add_element(app.alignment("rspace_around",
                         ft.Text("Primary Text", style=tools.primary_text_style(app, 18)),
@@ -30,11 +36,19 @@ def ui(app):
                         tools.dropdown(app, 12, label="Dropdown 1", options=[ft.DropdownOption("Option 1", "option1"), ft.DropdownOption("Option 2", "option2"), ft.DropdownOption("Option 3", "option3")], width=tools.adapt_dimensions(app, "w", 16), height=tools.adapt_dimensions(app, "h", 16)),
                         tools.slider(app),
                     expand=True))
-    app.add_element(app.alignment("rcenter",
-                        tools.radio(app, 12, label="Radio 1", width=tools.adapt_dimensions(app, "w", 16), height=tools.adapt_dimensions(app, "h", 16)),
-                        tools.checkbox(app, 12, label="Checkbox 1", width=tools.adapt_dimensions(app, "w", 16), height=tools.adapt_dimensions(app, "h", 16)),
-                        tools.switch(app, 12, label="Switch 1", width=tools.adapt_dimensions(app, "w", 16), height=tools.adapt_dimensions(app, "h", 16)),
+    app.add_element(app.alignment("rspace_evenly",
+                        tools.radio_group(app, tools.radio(app, 12, label="Radio 1", value="option1", width=tools.adapt_dimensions(app, "w", 10)), tools.radio(app, 12, label="Radio 2", value="option2", width=tools.adapt_dimensions(app, "w", 10))),
+                        tools.checkbox(app, 12, label="Checkbox", width=tools.adapt_dimensions(app, "w", 10)),
+                        tools.switch(app, 12, label="Switch", width=tools.adapt_dimensions(app, "w", 12)),
                     expand=True))
+    app.add_element(app.alignment("rspace_evenly",
+                        tools.progress_bar(app, width=tools.adapt_dimensions(app, "w", 32), height=tools.adapt_dimensions(app, "h", 4), value=0.7),
+                        tools.progress_ring(app, width=tools.adapt_dimensions(app, "w", 8), height=tools.adapt_dimensions(app, "w", 8), value=0.4),
+                    expand=True))
+    app.add_element(app.alignment("rcenter",
+                        table.build(),
+                    expand=True))
+    
     
 app = App()
 app.set_orientation("landscape")
